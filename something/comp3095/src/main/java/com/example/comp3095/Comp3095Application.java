f59a7408f3902f6ca9c4b29923d221d5bb2a9cfa
DerpyTheSpy <erickim334@gmail.com>
2022-09-23 09:49:48 -0400

    first commit

---
+++ something/comp3095/src/main/java/com/example/comp3095/Comp3095Application.java
@@ -0,0 +1,13 @@
+package com.example.comp3095;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3095Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp3095Application.class, args);
+	}
+
+}
---
+++ something/comp3095/src/test/java/com/example/comp3095/Comp3095ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comp3095;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3095ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
